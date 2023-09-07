ea355c8aa9cb5db875ad5fec2072d6d1b203e03b
ByalykT01 <byalykt@hotmail.com>
2023-09-07 19:29:54 +0200

    basic db usage, primitive task manager

--- mongodb.js
+++ mongodb.js
@@ -6,18 +6,51 @@ const client = new MongoClient(url)
 const dbName = "myProject"
 
 async function main(){
+  try{
   await client.connect()
   
   console.log("Connected succesfully")
+
   const db = client.db(dbName)
 
+  // const result = await db.collection('users').insertOne({
+  //   name: 'Taras',
+  //   age: 18
+  // })
 
-   await db.collection('users').insertOne({
-    name: 'Taras',
-    age: 18
-  })
+  //   console.log(result.insertedId)
 
-  return "done";
+  // const result = await db.collection('users').insertMany([
+  //   {
+  //     name: 'Bob',
+  //     age: 27
+  //   },
+  //   {
+  //     name: 'Jessie',
+  //     age: 24
+  //   }
+  // ])
+
+  // console.log(result.insertedIds)
+
+  const result = await db.collection('tasks').insertMany([
+    {
+      description: 'Polish lesson',
+      done: true
+    }, {
+      desciption: 'Daily walk',
+      done: false
+    }, {
+      description: 'Get fired',
+      done: true
+    }
+  ])
+  
+  console.log(result.insertedIds)
+  } catch(error) {
+  console.error("An error occured:", error)
+  } finally {
+  await client.close()
+  }
 }
-
 main()
