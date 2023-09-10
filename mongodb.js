051ec051b48f591efac5c4ae1235b1b701cb0705
ByalykT01 <byalykt@hotmail.com>
2023-09-10 21:07:23 +0200

    promises, documents update

--- mongodb.js
+++ mongodb.js
@@ -10,39 +10,25 @@ async function main(){
   await client.connect()
   console.log("Connected succesfully")
   const db = client.db(dbName)
-  const collection = db.collection('tasks')
 
-  const last = await collection.findOne(
-    {
-      _id: new ObjectId('64fa081e107d47fa78bf32c5')
-    }
-  )
-  if(last){
-    console.log(last)
-  }
-  else{
-    console.log("Error")
-  }
-
-  const done = await collection.find(
-    {
-      done: false
-    }
-  ).toArray()
-
-  if(done){
-    console.log(done)
-  }
-  else{
-    console.log("Error")
-  }
-
-  
-
+  const update = await db.collection('users').updateOne({
+      name: 'Oksana'
+    },{
+      $inc: {
+        age: 1
+      }
+})
+  console.log('Update successful!')
   } catch(error) {
-  console.error("An error occured:", error)
+  console.error("An error occurred:", error)
   } finally {
   await client.close()
   }
 }
 main()
+.then(() => {
+  console.log("Operation completed successfully.")
+})
+.catch(() => {
+  console.log("Operation failed:", error)
+})
