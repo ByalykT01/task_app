1111f676c951a24591b3ae71b1f80315bc1982f8
ByalykT01 <byalykt@hotmail.com>
2023-09-18 22:53:55 +0200

    Add task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,15 +2,3 @@ const mongoose = require('mongoose')
 
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api')
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  done: {
-    type: Boolean,
-    default: false
-  }
-})
