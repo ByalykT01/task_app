812bbff5dea1276df168c443e04bd4b43e609d9f
Taras Byalyk <byalykt@hotmail.com>
2023-09-25 21:58:27 +0000

    resource removing endpoint

--- src/index.js
+++ src/index.js
@@ -63,6 +63,20 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  const _id = req.params.id
+  try{
+    const user = await User.findByIdAndDelete(_id)
+    if(!user){
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch(e){
+    res.status(500).send()
+  }
+
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
   try{
@@ -91,7 +105,7 @@ app.get('/tasks/:id', async (req, res) => {
       }
       res.send(task)
   } catch(e){
-    res.status(500).send()
+    res.status(500).send(e)
   }
 })
 
@@ -114,7 +128,20 @@ app.patch('/tasks/:id', async (req, res) => {
     }
     res.send(task)
   } catch (e){
-    res.send(400).send(e)
+    res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+  try{
+  const task = await Task.findByIdAndDelete(_id)
+    if(!task){
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch(e){
+    res.status(500).send(e)
   }
 })
 
