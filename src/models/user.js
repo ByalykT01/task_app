a3faaaad6a1c5d459a59ba47a9873cf8aeb40758
Taras Byalyk <byalykt@hotmail.com>
2023-09-28 22:18:45 +0000

    json web token use

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema(
   {
@@ -39,9 +40,25 @@ const userSchema = new mongoose.Schema(
           throw new Error('Password must not be \'password\' ')
         }
       }
-    }
+    },
+    tokens: [{
+      token:{
+        type: String,
+        required: true
+      }
+    }]
   }
 )
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this 
+  const token = jwt.sign({ _id: user._id.toString() }, '7d3e28a53b914ae4451403779bcc1384b6e210668b2bafa6d69454c8de072226')
+  
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
